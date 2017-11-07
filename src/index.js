95eb02c30e957aa3b170e9a167bbb59f9bfb41e0
Federico Brigante <github@bfred.it>
2017-11-07 20:49:47 +0800

    Replace message-based ping with executeScript

--- src/index.js
+++ src/index.js
@@ -1,68 +1,47 @@
-import {pingContentScript} from 'webext-content-script-ping';
+// Mark this script as loaded
+// This is only meaningful when this module is loaded as a content_script
+document.__wasContentScriptLoaded = true;
 
-function logRuntimeErrors() {
-	if (chrome.runtime.lastError) {
-		console.error(chrome.runtime.lastError);
-	}
+async function p(fn, ...args) {
+	return new Promise((resolve, reject) => fn(...args, r => {
+		if (chrome.runtime.lastError) {
+			reject(chrome.runtime.lastError);
+		}
+		resolve(r);
+	}));
 }
 
-async function injectContentScript(script, tabId) {
-	const allFrames = script.all_frames;
-	const runAt = script.run_at;
-	script.css.forEach(file => chrome.tabs.insertCSS(tabId, {file, allFrames, runAt}, logRuntimeErrors));
-	script.js.forEach(file => chrome.tabs.executeScript(tabId, {file, allFrames, runAt}, logRuntimeErrors));
+function injectContentScriptsOnNewTabs() {
+	chrome.tabs.onUpdated.addListener((tabId, {status}) => {
+		if (status === 'loading') {
+			injectContentScripts(tabId);
+		}
+	});
 }
 
-async function injectContentScripts(tab) {
-	// Get the tab object if we don't have it already
-	if (!tab.id) {
-		tab = await new Promise(resolve => chrome.tabs.get(tab, resolve));
-		logRuntimeErrors();
-	}
-
-	// If we don't have the URL, we definitely can't access it.
-	if (!tab.url) {
-		return;
-	}
-
-	// We might just get the url because of the `tabs` permission,
-	// not necessarily because we have access to the origin.
-	// This will explicitly verify this permission.
-	const isPermitted = await new Promise(resolve => chrome.permissions.contains({
-		origins: [new URL(tab.url).origin + '/']
-	}, resolve));
-	logRuntimeErrors();
-
-	if (!isPermitted) {
-		return;
-	}
-
-	// Exit if already injected
-	try {
-		console.log('webext-dynamic-content-scripts: will ping');
-		console.log(tab.id || tab);
-		return await pingContentScript(tab.id || tab);
-	} catch (err) {
-		console.log('webext-dynamic-content-scripts: no ping back', err);
-	}
-
-	console.log('webext-dynamic-content-scripts: injecting');
-	chrome.runtime.getManifest().content_scripts.forEach(s => injectContentScript(s, tab.id));
-}
-
-export default function (tab = false) {
+export default async function injectContentScripts(tab = false) {
+	// Enable auto-mode
 	if (tab === false) {
-		console.log('webext-dynamic-content-scripts: will inject scripts on load');
-		chrome.tabs.onUpdated.addListener((tabId, {status}) => {
-			if (status === 'loading') {
-				console.log('webext-dynamic-content-scripts: will inject scripts, now loading');
-				console.log(tabId);
-				injectContentScripts(tabId);
-			}
+		return injectContentScriptsOnNewTabs();
+	}
+
+	// Really inject scripts on the specified tab
+	try {
+		const tabId = tab.id || tab;
+		const [hasScriptAlready] = await p(chrome.tabs.executeScript, tabId, {
+			code: 'document.__wasContentScriptLoaded',
+			runAt: 'document_start'
 		});
-	} else {
-		console.log('webext-dynamic-content-scripts: will inject scripts');
-		console.log(tab);
-		injectContentScripts(tab);
+		if (!hasScriptAlready) {
+			for (const group of chrome.runtime.getManifest().content_scripts) {
+				const allFrames = group.all_frames;
+				const runAt = group.run_at;
+				group.css.forEach(file => p(chrome.tabs.insertCSS, tabId, {file, allFrames, runAt}));
+				group.js.forEach(file => p(chrome.tabs.executeScript, tabId, {file, allFrames, runAt}));
+			}
+		}
+	} catch (err) {
+		// Probably the domain isn't permitted.
+		// It's easier to catch this than do 2 queries
 	}
 }

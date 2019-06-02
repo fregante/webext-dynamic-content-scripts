8e3a92d50030517bae23310fc2b7cc0b062013fd
Federico Brigante <github@bfred.it>
2019-06-02 18:03:11 +0800

    Verify the hostname before trying to inject it
    This check will automatically exclude any host already defined in manifest.json
    
    Now addToTab() will return a rejected promise if `tab` doesn't exist or doesn't have the correct permissions

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,18 @@
 /// <reference types="firefox-webext-browser" />
 
 /**
- * Same as `addToTab`, but it will automatically listen to new tabs and inject the scripts as needed.
+ * Same as `addToTab`, but it will automatically listen to new tabs and inject the scripts if they're allowed.
  *
- * @param contentScripts {string[]} An array of content scripts.
+ * @param contentScripts The content scripts to inject, follows the same format as in manifest.json
  */
-export function addToFutureTabs(contentScripts?: string[]): Promise<void>;
+export function addToFutureTabs(contentScripts?: ContentScript | ContentScript[]): void;
 
 /**
  * This is an advanced version of `chrome.tabs.executeScript`/`chrome.tabs.insertCSS`:
  * 	- It accepts a mixed JS/CSS object just like in `manifest.json`.
  *
- * @param tab {browser.tabs.Tab|number} Tab or tab ID to add the content scripts to.
- * @param contentScripts {string[]} An array of content scripts.
+ * @param tab Tab or tab ID to add the content scripts to. If missing or wrong, the returned promise will reject
+ * @param contentScripts The content scripts to inject, follows the same format as in manifest.json
  *
  * @example
  * 	DCS.addToTab(tab, {
@@ -30,4 +30,4 @@ export function addToFutureTabs(contentScripts?: string[]): Promise<void>;
  * 		// Not supported: all matches and globs properties
  * 	});
  */
-export function addToTab(tab: browser.tabs.Tab|number, contentScripts?: string[]): Promise<void>;
+export function addToTab(tab: browser.tabs.Tab | number, contentScripts?: ContentScript | ContentScript[]): Promise<void>;
--- src/index.js
+++ src/index.js
@@ -20,31 +20,59 @@ export async function addToTab(tab, contentScripts = chrome.runtime.getManifest(
 		contentScripts = [contentScripts];
 	}
 
-	try {
-		const tabId = tab.id || tab;
-		if (!await pingContentScript(tabId)) {
-			const injections = [];
-			for (const group of contentScripts) {
-				const allFrames = group.all_frames;
-				const runAt = group.run_at;
-				for (const file of group.css || []) {
-					injections.push(p(chrome.tabs.insertCSS, tabId, {file, allFrames, runAt}));
-				}
-				for (const file of group.js || []) {
-					injections.push(p(chrome.tabs.executeScript, tabId, {file, allFrames, runAt}));
-				}
+	const tabId = tab.id || tab;
+	if (!await pingContentScript(tabId)) {
+		const injections = [];
+		for (const group of contentScripts) {
+			const allFrames = group.all_frames;
+			const runAt = group.run_at;
+			for (const file of group.css || []) {
+				injections.push(p(chrome.tabs.insertCSS, tabId, {
+					file,
+					allFrames,
+					runAt
+				}));
 			}
-			return Promise.all(injections);
+
+			for (const file of group.js || []) {
+				injections.push(p(chrome.tabs.executeScript, tabId, {
+					file,
+					allFrames,
+					runAt
+				}));
+			}
 		}
-	} catch (err) {
-		// Probably the domain isn't permitted.
-		// It's easier to catch this than do 2 queries
+
+		return Promise.all(injections);
 	}
 }
 
+function getRegexFromGlobs(scripts) {
+	const regexStrings = [];
+	for (const script of scripts) {
+		for (const glob of script.matches) {
+			const regexString = glob
+				.replace(/^.*:\/\/([^/]+)\/.*/, '$1') // From `https://*.google.com/foo*bar` to `*.google.com`
+				.replace(/\./g, '\\.') // Escape dots
+				.replace(/\*/g, '.+'); // Converts `*` to `.+`
+			regexStrings.push('^' + regexString + '$');
+		}
+	}
+
+	return new RegExp(regexStrings.join('|'));
+}
+
 export function addToFutureTabs(scripts = chrome.runtime.getManifest().content_scripts) {
-	chrome.tabs.onUpdated.addListener((tabId, {status}) => {
-		if (status === 'loading') {
+	const hostsInScripts = getRegexFromGlobs(scripts);
+
+	chrome.tabs.onUpdated.addListener(async (tabId, {status}) => {
+		const {url} = await p(chrome.tabs.get, tabId);
+		if (!url) {
+			return; // No permission on domain
+		}
+
+		// Ensure that ontent script is already defined in manifest.json or `scripts` argument
+		if (status === 'loading' && !hostsInScripts.test(new URL(url).host)) {
 			addToTab(tabId, scripts);
 		}
 	});

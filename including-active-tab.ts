c742434844d59e53ecb8b59c2df2ec8db2506faa
Federico Brigante <me@fregante.com>
2023-01-09 01:32:39 +0800

    Reduce duplicate injection

--- including-active-tab.ts
+++ including-active-tab.ts
@@ -2,11 +2,20 @@
 import '.';
 import {type ContentScript} from 'webext-content-scripts/types';
 import {injectContentScript} from 'webext-content-scripts';
-import {type ActiveTab, onActiveTab} from './active-tab';
+import {type ActiveTab, onActiveTab} from './active-tab.js';
+import {isContentScriptRegistered} from './utils.js';
 
-function injectToTab(tab: ActiveTab): void {
-	if (tab.id) {
-		void injectContentScript(tab.id, chrome.runtime.getManifest().content_scripts as ContentScript);
+const scripts = chrome.runtime.getManifest().content_scripts as ContentScript;
+
+async function injectToTab({id, origin}: ActiveTab): Promise<void> {
+	if (id && await isContentScriptRegistered(origin)) {
+		// Warning: This will still cause duplicate injection on frames of activeTabs with origins that have "registered" content scripts.
+		// Example:
+		// 1. manifest injects on google.com
+		// 2. user enables activeTab on example.com
+		// 3. the tab has a Google Maps frame that already has native content scripts
+		// 4. this line will inject the content script on both frame 0 and the Google Maps frame
+		void injectContentScript(id, scripts);
 	}
 }
 

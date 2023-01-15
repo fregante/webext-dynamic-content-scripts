a74ff33dd40db812fc30bcf81c89a890c8ae89a6
Federico Brigante <me@fregante.com>
2023-01-15 13:56:49 +0800

    Unit tests trials

---
+++ jest.setup.js
@@ -0,0 +1,5 @@
+import chrome from 'sinon-chrome'
+
+global.chrome = chrome;
+
+jest.mock('webext-additional-permissions');
---
+++ test/unit.js
@@ -0,0 +1,18 @@
+import chrome from 'sinon-chrome';
+
+import {describe, it, expect, test, jest} from '@jest/globals';
+jest.mock('webext-additional-permissions', () => {});
+// const {getAdditionalPermissions} = await import('webext-additional-permissions');
+
+describe('core', () => {
+	it('should load page', async () => {
+		const manifest = {
+			name: 'my chrome extension',
+			manifest_version: 2,
+			version: '1.0.0',
+		};
+
+		await import('../index.js');
+		expect(getAdditionalPermissions).toHaveBeenCalled();
+	});
+});

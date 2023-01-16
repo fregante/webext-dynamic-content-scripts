ea1c0cc6ebf2b05b9d087c9a771bc2547e14ccee
Federico Brigante <me@fregante.com>
2023-01-16 16:04:52 +0800

    Extract `injectToExistingTabs` to `webext-content-scripts`

--- source/inject-to-existing-tabs.ts
+++ source/inject-to-existing-tabs.ts
@@ -1,19 +1,11 @@
-import {injectContentScript} from 'webext-content-scripts';
+import {getTabsByUrl, injectContentScript} from 'webext-content-scripts';
 
-export function injectToExistingTabs(
+export async function injectToExistingTabs(
 	origins: string[],
-	scripts: ManifestContentScripts) {
-	if (origins.length === 0) {
-		return;
-	}
-
-	chrome.tabs.query({
-		url: origins,
-	}, tabs => {
-		for (const tab of tabs) {
-			if (tab.id) {
-				void injectContentScript(tab.id, scripts);
-			}
-		}
-	});
+	scripts: ManifestContentScripts,
+) {
+	return injectContentScript(
+		await getTabsByUrl(origins),
+		scripts,
+	);
 }

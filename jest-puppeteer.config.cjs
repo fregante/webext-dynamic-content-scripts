90be2aa83faf1fbe8b51ede7ac5d9a2d27c344f4
Federico Brigante <me@fregante.com>
2023-05-16 23:59:05 +0800

    Meta: Drop publishing dependency

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
 	launch: {
 		headless: false,
 		args: [
-			'--disable-extensions-except=' + path.resolve(__dirname, 'test/dist/mv' + process.env.TARGET),
+			'--disable-extensions-except=' + path.resolve(__dirname, 'test/dist/mv' + (process.env.TARGET ?? 2)),
 			'--window-size=400,800',
 		],
 	},

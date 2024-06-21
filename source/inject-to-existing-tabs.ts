94d689a7b5d9b5cad52ff193088e36b424b35744
Federico <me@fregante.com>
2024-06-21 16:53:39 +0800

    Couple more notes

--- source/inject-to-existing-tabs.ts
+++ source/inject-to-existing-tabs.ts
@@ -2,6 +2,8 @@ import {getTabsByUrl, injectContentScript} from 'webext-content-scripts';
 
 type ManifestContentScripts = NonNullable<chrome.runtime.Manifest['content_scripts']>;
 
+// May not be needed in the future in Firefox
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1458947
 export async function injectToExistingTabs(
 	origins: string[],
 	scripts: ManifestContentScripts,

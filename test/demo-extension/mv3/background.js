c81ce93d02ce6bd0cda6410898f32653ddef4eb5
Federico <me@fregante.com>
2025-01-31 16:02:25 +0900

    Drop `activeTab` support

--- test/demo-extension/mv3/background.js
+++ test/demo-extension/mv3/background.js
@@ -1,15 +1,3 @@
-import 'webext-dynamic-content-scripts/including-active-tab.ts';
-
-chrome.contextMenus.create({
-	title: 'Enable activeTab permission',
-}, () => {
-	if (chrome.runtime.lastError) {
-		// Shush
-	}
-});
-
-chrome.contextMenus.onClicked.addListener(() => {
-	console.log('Context menu clicked');
-});
+import 'webext-dynamic-content-scripts';
 
 console.log('Background loaded');

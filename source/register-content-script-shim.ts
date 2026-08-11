c8a80884cfae0be5aaa0e13467a74ffbffeb8168
Federico <me@fregante.com>
2026-08-11 16:56:35 +0800

    shush ts

--- source/register-content-script-shim.ts
+++ source/register-content-script-shim.ts
@@ -6,7 +6,7 @@ export const firefoxRegister = globalThis.browser?.contentScripts?.register;
 export async function registerContentScript(
 	contentScript: Omit<chrome.scripting.RegisteredContentScript, 'id' | 'world'> & {matches: string[]},
 ): Promise<browser.contentScripts.RegisteredContentScript> {
-	if (chromeRegister) {
+	if (chromeRegister !== undefined) {
 		const id = 'webext-dynamic-content-script-' + JSON.stringify(contentScript);
 		try {
 			// Don't use `chromeRegister` directly https://github.com/fregante/webext-dynamic-content-scripts/pull/80
